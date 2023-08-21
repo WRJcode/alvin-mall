c39add48f65877251de1157583feb39795e0a697
Alvin <wrjcode@163.com>
2023-08-21 09:01:05 +0800

    新增api工具包，包括结果、错误码、分页

---
+++ common/src/main/java/org/wrj/common/api/CommonPage.java
@@ -0,0 +1,25 @@
+package org.wrj.common.api;
+
+import java.util.List;
+
+public class CommonPage<T> {
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+
+    private Integer totalPage;
+
+    private Long total;
+
+    private List<T> list;
+
+    /**
+     * 将PageHelper分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(List<T> list){
+        CommonPage<T> result = new CommonPage<T>();
+
+        return null;
+    }
+}
---
+++ common/src/main/java/org/wrj/common/api/IErrorCode.java
@@ -0,0 +1,7 @@
+package org.wrj.common.api;
+
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
