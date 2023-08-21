b8e9af0838a51b3eadc056dc209be0554bc78afd
Alvin <wrjcode@163.com>
2023-08-21 17:58:42 +0800

    公共模块Common中新增全局异常类、断言类、自定义异常

---
+++ common/src/main/java/org/wrj/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.wrj.common.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.wrj.common.api.CommonResult;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonResult handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField()+fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
+    }
+}
